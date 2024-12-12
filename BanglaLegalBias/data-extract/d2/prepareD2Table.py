1b2702b6a5b3ce5937816310059fc3ae68c3ef39
karit7 <120469589+karit7@users.noreply.github.com>
2024-12-13 05:26:19 +0600

    prepareD2Table.py updated

--- BanglaLegalBias/data-extract/d2/prepareD2Table.py
+++ BanglaLegalBias/data-extract/d2/prepareD2Table.py
@@ -27,13 +27,13 @@ def separate(df):
 			df[column] = df[column].str.replace("/", "; ")
 
 
-tjmg1 = pd.read_csv("jrmg1_violence.csv")
-tjmg2 = pd.read_csv("jrmg2_violence.csv")
-tjrj = pd.read_csv("jrrj_violence.csv")
-tjsp1 = pd.read_csv("jrsp1_violence.csv")
-tjsp2 = pd.read_csv("jrsp2_violence.csv")
+jr3 = pd.read_csv("jr3_violence.csv")
+jr4 = pd.read_csv("jr4_violence.csv")
+jr = pd.read_csv("jr_violence.csv")
+jr1 = pd.read_csv("jr1_violence.csv")
+jr2 = pd.read_csv("jr2_violence.csv")
 
-df_list = [tjmg1, tjmg2, tjrj, tjsp1, tjsp2]
+df_list = [jr3, jr4, jr, jr1, jr2]
 
 for item in df_list:
 	item.drop(["Unnamed: 0"], axis=1, inplace=True)
@@ -51,8 +51,8 @@ for item in df_list:
 
 	item.sort_values('date_judgment', inplace=True, ignore_index=True)
 
-tjmg1.to_csv("jrmg1_annotate.csv", escapechar=";")
-tjmg2.to_csv("jrmg2_annotate.csv", escapechar=";")
-tjrj.to_csv("jrrj_annotate.csv", escapechar=";")
-tjsp1.to_csv("jrsp1_annotate.csv", escapechar=";")
-tjsp2.to_csv("jrsp2_annotate.csv", escapechar=";")
+jr3.to_csv("jr3_annotate.csv", escapechar=";")
+jr4.to_csv("jr4_annotate.csv", escapechar=";")
+jr.to_csv("jr_annotate.csv", escapechar=";")
+jr1.to_csv("jr1_annotate.csv", escapechar=";")
+jr2.to_csv("jr2_annotate.csv", escapechar=";")
